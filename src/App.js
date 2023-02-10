3e6d97357345c5912ccef80ed6ffb44738e3f7e2
CoderHouse-24915 <disalvo.juan.daniel@gmail.com>
2023-02-10 11:16:52 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+import axios from "axios";
+
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+
+//Components
+import Header from "./components/Header/Header";
+import CardUser from "./components/CardUser/CardUser";
+
+const App = () => {
+  const baseURL = "https://api.github.com/users/";
+
+  const [value, setValue] = useState("");
+  const [userData, setUserData] = useState({});
+
+  // operador ternario | ternary operator
+  // condición ? expr1 : expr2
+  // condición ? true : false
+
+  const onChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const userInput = value.toLowerCase().replace(/ /g, ""); // sanitizamos la variable de espacios
+    if (userInput) {
+      axios(`${baseURL}${userInput}`).then((res) => setUserData(res.data));
+    }
+    setValue("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Button
+        variant="contained"
+        onClick={() => alert("Hello World")}
+        sx={{ mt: 4 }}
+      >
+        Contained
+      </Button>
+      <form className="Form" onSubmit={onSubmit}>
+        <TextField
+          placeholder="Buscar usuario"
+          variant="outlined"
+          className="Textfield"
+          value={value}
+          onChange={onChange}
+        />
+        <button className="btn" type="submit">
+          Buscar
+        </button>
+      </form>
+
+      {userData.id ? <CardUser userData={userData} /> : null}
     </div>
   );
-}
+};
 
 export default App;
